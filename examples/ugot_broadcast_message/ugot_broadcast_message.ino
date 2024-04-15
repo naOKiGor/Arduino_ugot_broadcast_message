512baec1704f56df592a426aa3f6aca19d7e7e01
naOKi Chan <naoki.chan@ubtrobot.com>
2024-04-15 17:34:42 +0800

    first commit

---
+++ examples/ugot_broadcast_message/ugot_broadcast_message.ino
@@ -0,0 +1,50 @@
+#include <M5StickC.h>
+#include <WiFi.h>
+#include <ugot_broadcast_message.h>
+
+// const char* ssid = "REPLACE_WITH_YOUR_SSID";
+// const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "UBT-Robot";
+const char* password = "ubtubtubt";
+
+static uGotBroadcastChannel channel;
+
+void setup() {
+  M5.begin();
+
+  // Delete old wifi config
+  WiFi.disconnect(true);
+
+  delay(1000);
+
+  Serial.printf("Connecting to WiFi[%s] ...\n", ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.println("WiFi connect failure");
+      while(1) {
+          delay(1000);
+      }
+  }
+
+  Serial.printf("WiFi connected, IP address: %s\n", WiFi.localIP().toString().c_str());
+
+  // Optional set communication channel, default is 0
+  // channel.setChannel(0);
+
+  // Enable broadcast funtion, default is off
+  channel.setEnable(true);
+
+  // Set broadcast message received handler
+  channel.setMessageReceivedCallback([](const char* msg) {
+    Serial.printf("Received Message: %s\n", msg);
+	});
+}
+
+void loop() {
+  delay(5000);
+
+  // Send broadcast to current channel
+  channel.sendBroadcastMessage("Hello world");
+}
---
+++ src/ugot_broadcast_message.h
@@ -0,0 +1,191 @@
+#ifndef _UGOT_BROADCAST_MESSAGE_H_
+#define _UGOT_BROADCAST_MESSAGE_H_
+
+#include <AsyncUDP.h>
+#include <nanopb/pb_encode.h>
+#include <nanopb/pb_decode.h>
+#include <broadcast_message.pb.h>
+
+#define BOARDCAST_DEFAULT_CHANNEL_ID   0
+#define BOARDCAST_MAX_CHANNEL_COUNT    100
+#define BOARDCAST_CHANNEL_BASEID       25880
+
+#define HEADER_INDICATE                0xDB
+
+class uGotBroadcastChannel
+{
+public:
+	uGotBroadcastChannel() {
+		this->callback = nullptr;
+		this->channel = BOARDCAST_DEFAULT_CHANNEL_ID;
+		this->enable = false;
+	}
+
+	virtual ~uGotBroadcastChannel() {
+		releaseUdp();
+	};
+
+public:
+	bool sendBroadcastMessage(const char* message) {
+		return sendBroadcastMessageImpl(message);
+	}
+
+	void setMessageReceivedCallback(std::function<void(const char*)> callback) {
+		this->callback = callback;
+	}
+	
+	bool isEnable() {
+		return enable;
+	}
+	
+	void setEnable(bool enable) {
+		this->enable = enable;
+		if(enable) {
+			initUdp();
+		} else {
+			releaseUdp();
+		}
+	}
+	
+	uint16_t getPort() {
+		return BOARDCAST_CHANNEL_BASEID + channel;
+	}
+	
+	uint8_t getChannel() {
+		return channel;
+	}
+	
+	void setChannel(uint8_t channel) {
+		if(channel < 0 || channel >= BOARDCAST_MAX_CHANNEL_COUNT) {
+			return;
+		}
+		this->channel = channel;
+		initUdp();
+	}
+	
+protected:
+	void initUdp() {
+		if(!enable) {
+			return;
+		}
+		releaseUdp();
+		delay(1000);
+		if(udp.listen(getPort())) {
+			Serial.printf("UDP listening: %s:%d\n", udp.listenIP().toString().c_str(), getPort());
+			udp.onPacket([this](AsyncUDPPacket packet) {
+				this->onReceivedPacket(packet);
+			});
+		} else {
+			Serial.printf("UDP listen failure\n");
+		}
+	}
+	
+	void releaseUdp() {
+		udp.close();
+	}
+	
+	uint8_t crc_checksum(uint8_t* data, size_t offset, size_t length) {
+        int32_t crc = 0;
+        for(int i = 0; i < length; i++) {
+            crc ^= data[i + offset];
+            for(int j = 0; j < 8; j++) {
+                if(crc & 0x80) {
+                    crc = crc << 1 ^ 0x07;
+                } else {
+                    crc = crc << 1;
+				}
+			}
+		}
+        return (crc ^ 0x55) & 0xFF;
+	}
+	
+	bool sendBroadcastMessageImpl(const char* message) {
+		if(!enable) return false;
+		
+		uint8_t buffer[128] = {0};
+		uint8_t payload_buffer[128] = {0};
+		pb_ostream_t stream;
+		bool status;
+		
+		broadcast_message_header header = {
+			.dev = 2,
+			.cmd = 2700,
+			.attr = broadcast_message_header_Attr_PUSH
+		};
+		
+		broadcast_message_payload payload;
+		strncpy(payload.name, "BMSG", sizeof(payload.name));
+		strncpy(payload.value, message, sizeof(payload.value));
+		
+		stream = pb_ostream_from_buffer(payload_buffer, sizeof(payload_buffer));
+		status = pb_encode(&stream, broadcast_message_payload_fields, &payload);
+		if (!status) return false;
+		
+		uint8_t payload_size = stream.bytes_written;
+		header.dataLen = payload_size;
+		
+		stream = pb_ostream_from_buffer(&buffer[2], sizeof(buffer) - 2);
+		status = pb_encode(&stream, broadcast_message_header_fields, &header);
+		if (!status) return false;
+		
+		uint8_t header_size = stream.bytes_written;
+		
+		buffer[0] = HEADER_INDICATE;
+		buffer[1] = header_size;
+		buffer[2 + header_size] = crc_checksum(&buffer[2], 0, header_size);
+		memcpy(&buffer[3 + header_size], payload_buffer, payload_size);
+		buffer[3 + header_size + payload_size] = crc_checksum(payload_buffer, 0, payload_size);
+		
+		udp.broadcastTo(buffer, 4 + header_size + payload_size, getPort());
+		
+		return true;
+	}
+	
+	void onReceivedPacket(AsyncUDPPacket packet) {
+		pb_istream_t stream;
+		bool status;
+		
+		uint8_t* data = packet.data();
+		size_t data_size = packet.length();
+		
+		if(data_size < 3) return;
+		
+		if(data[0] != HEADER_INDICATE) return;
+		
+		uint8_t header_size = data[1];
+		
+		if(data_size < 3 + header_size) return;
+		
+		if(crc_checksum(&data[2], 0, header_size) != data[2 + header_size]) return;
+		
+		broadcast_message_header header;
+		stream = pb_istream_from_buffer(&data[2], header_size);
+		status = pb_decode(&stream, broadcast_message_header_fields, &header);
+		if (!status) return;
+		
+		if(header.attr != broadcast_message_header_Attr_PUSH || header.dev != 2 || header.cmd != 2700) return;
+		
+		size_t payload_size = header.dataLen;
+		
+		if(data_size < 4 + header_size + payload_size) return;
+		
+		if(crc_checksum(&data[3 + header_size], 0, payload_size) != data[3 + header_size + payload_size]) return;
+		
+		broadcast_message_payload payload;
+		stream = pb_istream_from_buffer(&data[3 + header_size], payload_size);
+		status = pb_decode(&stream, broadcast_message_payload_fields, &payload);
+		if (!status) return;
+		
+		if(this->callback != nullptr) {
+			this->callback(payload.value);
+		}
+	}
+
+private:
+	std::function<void(const char*)> callback;
+	uint8_t channel;
+	bool enable;
+	AsyncUDP udp;
+};
+
+#endif
